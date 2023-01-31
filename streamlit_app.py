15e49ac11c8ae14c02dbf10e81c9f9b42accb535
volamr <volamr@users.noreply.github.com>
2023-01-31 17:43:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-Breakfast Menu
+streamlit.header('Breakfast Menu')
 
-Omega 3 & Blueberry Oatmeal
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-Kale, Spinach & Rocket Smotthie
+streamlit.text('Kale, Spinach & Rocket Smotthie')
 
-Hard-Boiled Free-Range Egg
+streamlit.text('Hard-Boiled Free-Range Egg')

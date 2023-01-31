4c5982c1536006308bd575dcceb62f029e9a23c8
volamr <volamr@users.noreply.github.com>
2023-01-31 17:59:22 -0500

    Update streamlit_app.py
    Added pandas & my fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Mom`s New Healthy Diner')
 
@@ -14,3 +15,6 @@ streamlit.text('🥑Avacado Toast')
 
 
 streamlit.header('🍌🍍Build Your Own Smoothie🥕🍒🍏🍉')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
